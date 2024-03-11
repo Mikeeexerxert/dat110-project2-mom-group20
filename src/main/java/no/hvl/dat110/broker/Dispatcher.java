93a9c17362f42c05479b2d0614372caf36769299
Bender <ahmad.jahan2001@gmail.com>
2024-03-11 17:09:14 +0100

    Version 1

--- src/main/java/no/hvl/dat110/broker/Dispatcher.java
+++ src/main/java/no/hvl/dat110/broker/Dispatcher.java
@@ -1,162 +1,112 @@
 package no.hvl.dat110.broker;
-
-import java.util.Set;
 import java.util.Collection;
-
-import no.hvl.dat110.common.TODO;
 import no.hvl.dat110.common.Logger;
 import no.hvl.dat110.common.Stopable;
 import no.hvl.dat110.messages.*;
 import no.hvl.dat110.messagetransport.Connection;
-
 public class Dispatcher extends Stopable {
-
-	private Storage storage;
-
+	private final Storage storage;
 	public Dispatcher(Storage storage) {
 		super("Dispatcher");
 		this.storage = storage;
-
 	}
-
 	@Override
 	public void doProcess() {
-
 		Collection<ClientSession> clients = storage.getSessions();
-
 		Logger.lg(".");
 		for (ClientSession client : clients) {
-
 			Message msg = null;
-
 			if (client.hasData()) {
 				msg = client.receive();
 			}
-
 			// a message was received
 			if (msg != null) {
-				dispatch(client, msg);
+				dispatch(msg);
 			}
 		}
-
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-
-	public void dispatch(ClientSession client, Message msg) {
-
+	public void dispatch(Message msg) {
 		MessageType type = msg.getType();
-
 		// invoke the appropriate handler method
 		switch (type) {
-
 		case DISCONNECT:
 			onDisconnect((DisconnectMsg) msg);
 			break;
-
 		case CREATETOPIC:
 			onCreateTopic((CreateTopicMsg) msg);
 			break;
-
 		case DELETETOPIC:
 			onDeleteTopic((DeleteTopicMsg) msg);
 			break;
-
 		case SUBSCRIBE:
 			onSubscribe((SubscribeMsg) msg);
 			break;
-
 		case UNSUBSCRIBE:
 			onUnsubscribe((UnsubscribeMsg) msg);
 			break;
-
 		case PUBLISH:
 			onPublish((PublishMsg) msg);
 			break;
-
 		default:
 			Logger.log("broker dispatch - unhandled message type");
 			break;
-
 		}
 	}
-
 	// called from Broker after having established the underlying connection
 	public void onConnect(ConnectMsg msg, Connection connection) {
-
-		String user = msg.getUser();
-
-		Logger.log("onConnect:" + msg.toString());
-
-		storage.addClientSession(user, connection);
-
+		storage.addClientSession(msg.getUser(), connection);
+		Logger.log("onConnect: " + msg);
 	}
-
 	// called by dispatch upon receiving a disconnect message
 	public void onDisconnect(DisconnectMsg msg) {
-
-		String user = msg.getUser();
-
-		Logger.log("onDisconnect:" + msg.toString());
-
-		storage.removeClientSession(user);
-
+		storage.removeClientSession(msg.getUser());
+		Logger.log("onDisconnect: " + msg);
 	}
-
 	public void onCreateTopic(CreateTopicMsg msg) {
-
-		Logger.log("onCreateTopic:" + msg.toString());
-
-		// TODO: create the topic in the broker storage
-		// the topic is contained in the create topic message
-
-		throw new UnsupportedOperationException(TODO.method());
-
+		storage.createTopic(msg.getTopic());
+		Logger.log("onCreateTopic: " + msg);
 	}
-
 	public void onDeleteTopic(DeleteTopicMsg msg) {
-
-		Logger.log("onDeleteTopic:" + msg.toString());
-
-		// TODO: delete the topic from the broker storage
-		// the topic is contained in the delete topic message
-		
-		throw new UnsupportedOperationException(TODO.method());
+		storage.deleteTopic(msg.getTopic());
+		Logger.log("onDeleteTopic: " + msg);
 	}
-
 	public void onSubscribe(SubscribeMsg msg) {
-
-		Logger.log("onSubscribe:" + msg.toString());
-
-		// TODO: subscribe user to the topic
-		// user and topic is contained in the subscribe message
-		
-		throw new UnsupportedOperationException(TODO.method());
-
+		/**Logger.log("onSubscribe: Start - User: " + msg.getUser() + ", Topic: " + msg.getTopic());
+		storage.addSubscriber(msg.getTopic(), msg.getUser());
+		Collection<String> subscribers = storage.getSubscribers(msg.getTopic());
+		Logger.log("onSubscribe: Subscribers for topic " + msg.getTopic() + " - " + subscribers);
+		Logger.log("onSubscribe: End");**/
+		storage.addSubscriber(msg.getTopic(), msg.getUser());
+		Logger.log("onSubscribe: " + msg.getUser() + " subscribed to " + msg.getTopic());
 	}
-
 	public void onUnsubscribe(UnsubscribeMsg msg) {
-
-		Logger.log("onUnsubscribe:" + msg.toString());
-
-		// TODO: unsubscribe user to the topic
-		// user and topic is contained in the unsubscribe message
-		
-		throw new UnsupportedOperationException(TODO.method());
+		storage.removeSubscriber(msg.getTopic(), msg.getUser());
+		Logger.log("onUnsubscribe: " + msg.getUser() + " unsubscribed from " + msg.getTopic());
 	}
-
 	public void onPublish(PublishMsg msg) {
+		/**Logger.log("onPublish: Start");
+		Collection<String> subscribers = storage.getSubscribers(msg.getTopic());
+		Logger.log("onPublish: Subscribers for topic " + msg.getTopic() + " - " + subscribers);
+		for (String subscriber : subscribers) {
+			ClientSession session = storage.getSession(subscriber);
+			Logger.log("onPublish: Session for subscriber " + subscriber + " - " + session);
+			if (session != null) {
+				session.send(msg);
+				Logger.log("onPublish: Message sent to " + subscriber);
+			}
+			else {
+				Logger.log("onPublish: No session for subscriber " + subscriber);
+			}
+		}
+		Logger.log("onPublish: " + msg);
+		Logger.log("onPublish: End");**/
 
-		Logger.log("onPublish:" + msg.toString());
-
-		// TODO: publish the message to clients subscribed to the topic
-		// topic and message is contained in the subscribe message
-		// messages must be sent using the corresponding client session objects
-		
-		throw new UnsupportedOperationException(TODO.method());
-
+		storage.getSubscribers(msg.getTopic()).forEach(subscriber -> storage.getSession(subscriber).send(msg));
+		Logger.log("onPublish: " + msg);
 	}
 }
--- src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -1,30 +1,24 @@
 package no.hvl.dat110.iotsystem;
-
 import no.hvl.dat110.client.Client;
-import no.hvl.dat110.common.TODO;
-
 public class TemperatureDevice {
-
 	private static final int COUNT = 10;
-
 	public static void main(String[] args) {
-
 		// simulated / virtual temperature sensor
 		TemperatureSensor sn = new TemperatureSensor();
-
-		// TODO - start
-
-		// create a client object and use it to
-
-		// - connect to the broker - user "sensor" as the user name
-		// - publish the temperature(s)
-		// - disconnect from the broker
-
-		// TODO - end
-
+		Client client = new Client("sensor", Common.BROKERHOST, Common.BROKERPORT);
+		client.connect();
+		for (int i = 0; i < COUNT; i++) {
+			int temp = sn.read();
+			System.out.println("Temperature: " + temp);
+			client.publish(Common.TEMPTOPIC, Integer.toString(temp));
+			try {
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		client.disconnect();
 		System.out.println("Temperature device stopping ... ");
-
-		throw new UnsupportedOperationException(TODO.method());
-
 	}
 }
